850480f8efefe56b034c93e9238446723d82d6f0
Deepak Sharma <sharma.deepak8616@gmail.com>
2020-09-24 08:57:22 -0400

    review comments incorporated

--- src/test/java/TestPkg/TestClass.java
+++ src/test/java/TestPkg/TestClass.java
@@ -5,12 +5,12 @@ import org.testng.annotations.Test;
 public class TestClass {
 
     @Test
-    public void test1() {
+    public void testMethod1() {
         System.out.println("Test1");
     }
 
     @Test
-    public void test2() {
+    public void testMethod2() {
         System.out.println("Test2");
     }
 
