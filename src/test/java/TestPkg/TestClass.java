ecdd85d90d342dddef6e7c04d448113ecdceba8d
Deepak Sharma <sharma.deepak8616@gmail.com>
2020-09-24 08:52:15 -0400

    code in src

---
+++ src/test/java/TestPkg/TestClass.java
@@ -0,0 +1,17 @@
+package TestPkg;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+    @Test
+    public void test1() {
+        System.out.println("Test1");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("Test2");
+    }
+
+}
