4d841a8fe0af1ef6d8c0c296f6dc39ba2eba2e4d
Deepak Sharma <sharma.deepak8616@gmail.com>
2020-09-28 10:01:29 -0400

    enhanced framework

---
+++ src/test/java/Testcases/SignIn.java
@@ -0,0 +1,14 @@
+package Testcases;
+
+public class SignIn {
+
+    // user is able to create new teacher account
+
+    // user is able to create new parent account
+
+    // user is not able to move ahead without giving mandatory information
+
+    //
+
+
+}
