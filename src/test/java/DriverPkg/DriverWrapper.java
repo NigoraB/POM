db1f8a5b88c8eb8535226d33cd15b9e2aa95f48f
Deepak Sharma <sharma.deepak8616@gmail.com>
2020-10-01 10:03:11 -0400

    scroll and wait

--- src/test/java/DriverPkg/DriverWrapper.java
+++ src/test/java/DriverPkg/DriverWrapper.java
@@ -3,6 +3,8 @@ package DriverPkg;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class DriverWrapper {
 
     private static WebDriver driver;
@@ -10,7 +12,10 @@ public class DriverWrapper {
     public void initDriver() {
         System.setProperty("webdriver.chrome.driver", "./driverExec/chromedriver");
         driver = new ChromeDriver();
-        driver.get("https://clubs3qa1.scholastic.com/");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        // driver.get("https://clubs3qa1.scholastic.com/");
+        driver.get("https://www.hotels.com/");
+
     }
 
     public void quitDriver() {
--- src/test/java/Pages/Basepage.java
+++ src/test/java/Pages/Basepage.java
@@ -1,24 +1,52 @@
 package Pages;
 
 import DriverPkg.DriverWrapper;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import com.google.common.base.Function;
+import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.Wait;
 
 import java.sql.Driver;
+import java.time.Duration;
 import java.util.List;
 import java.util.Set;
 
 public class Basepage {
 
     public List<WebElement> findWebElements(By locator) {
-        return DriverWrapper.getDriver().findElements(locator);
+        Wait fWait = new FluentWait(DriverWrapper.getDriver())
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofSeconds(1))
+                .ignoring(NoAlertPresentException.class)
+                .ignoring(NoSuchElementException.class)
+                .withMessage("Element is not found after 30seconds of wait");
+
+        List<WebElement> elements = (List<WebElement>) fWait.until(new Function<WebDriver, List<WebElement>>() {
+            public List<WebElement> apply(WebDriver driver) {
+                return driver.findElements(locator);
+            }
+        });
+        return elements;
     }
 
-    public WebElement findWebElement(By locator) {
-        return DriverWrapper.getDriver().findElement(locator);
+    public WebElement findWebElement(final By locator) {
+        Wait fWait = new FluentWait(DriverWrapper.getDriver())
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofSeconds(1))
+                .ignoring(NoAlertPresentException.class)
+                .ignoring(NoSuchElementException.class)
+                .ignoring(StaleElementReferenceException.class)
+                .withMessage("Element is not found after 30seconds of wait");
+
+        WebElement element = (WebElement) fWait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(locator);
+            }
+        });
+        return element;
+        // return DriverWrapper.getDriver().findElement(locator);
     }
 
     public void type(By locator, String data) {
@@ -124,6 +152,32 @@ public class Basepage {
                 break;
             }
         }
+    }
+
+    public void scrollDownByPixel(int y) {
+        JavascriptExecutor js = (JavascriptExecutor)DriverWrapper.getDriver();
+        js.executeScript("scrollBy(0,"+y+");");
+    }
+
+    public void scrollUpByPixel(int y) {
+        JavascriptExecutor js = (JavascriptExecutor)DriverWrapper.getDriver();
+        js.executeScript("scrollBy(0,-"+y+");");
+    }
+
+    public void MoveAroundByPixel(int x, int y) {
+        JavascriptExecutor js = (JavascriptExecutor)DriverWrapper.getDriver();
+        js.executeScript("scrollBy("+x+","+y+");");
+    }
+
+    public void scrollUptoElement(By locator) {
+        WebElement element = findWebElement(locator);
+        JavascriptExecutor js = (JavascriptExecutor)DriverWrapper.getDriver();
+        js.executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    public void scrollToBottom() {
+        JavascriptExecutor js = (JavascriptExecutor)DriverWrapper.getDriver();
+        js.executeScript("window.scrollTo(0,document.body.scrollHeight);");
     }
 
 
